5c25f4c82592e360e890d06f5f577b80a39b925a
Vedant coding <100594881+Mrvedant99@users.noreply.github.com>
2024-10-22 20:58:59 +0530

    Create sign in API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -29,6 +29,6 @@ export const signin = async (req, res, next) => {
         res.cookie('access_token', token, { httpOnly: true}).status(200).json(rest);
     } catch (error) {
         next(error);
-    }
+    } 
 }
 
