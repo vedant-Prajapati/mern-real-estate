ff9a5323a83d0b2ae2db63823b28aaf8febd8fb2
Vedant coding <100594881+Mrvedant99@users.noreply.github.com>
2024-10-26 20:27:47 +0530

    Update the header and make the profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,25 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import About from './pages/About';
-import Profile from './pages/Profile';
-import SignUp from './pages/SignUp';
-import Header from './components/Header';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
+import PrivateRoute from "./components/privateRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header/>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/sign-in' element={<SignIn/>}/>
-        <Route path='/sign-up' element={<SignUp/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/profile' element={<Profile/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,16 @@
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <header className=" bg-slate-300 shadow-md">
       <div className=" flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className=" font-bold text-sm sm:text-xl flex flex-wrap">
             <span className=" text-red-500">MARVEL</span>
-            <span className=" text-slate-700">Home</span>
+            <span className=" text-slate-700">Estate</span>
           </h1>
         </Link>
         <form className=" bg-slate-100 p-3 rounded-lg flex items-center">
@@ -20,20 +22,29 @@ export default function Header() {
           <FaSearch className=" text-slate-600" />
         </form>
         <ul className=" flex gap-4">
-        <Link to='/'>
-          <li className=" hidden sm:inline text-slate-700 hover:underline">
-            Home
-          </li>
+          <Link to="/">
+            <li className=" hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
           </Link>
-          <Link to='about'>
-          <li className=" hidden sm:inline text-slate-700 hover:underline">
-            About
-          </li>
+          <Link to="about">
+            <li className=" hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
           </Link>
-          <Link to='sign-in'>
-          <li className=" sm:inline text-slate-700 hover:underline">
-            Sign in
-          </li>
+
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                className=" rounded-full h-8 w-8 object-cover "
+                src={currentUser.avatar}
+                alt="profile"
+              />
+            ) : (
+              <li className=" sm:inline text-slate-700 hover:underline">
+                Sign in
+              </li>
+            )}
           </Link>
         </ul>
       </div>
